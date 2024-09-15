13d59b3fffbe85b8746eea41de190210037b98ba
abdullah1610 <shaikhabdullah1610@gmail.com>
2024-09-15 22:37:10 +0530

    46. capturing user credentials using forms part 2,working

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,16 @@
+# INCREMENTAL CODE FOR SECTION : 11 LECTURE : 44 - VALIDATING FORMS
+# auth/forms.py
+
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Whats your name')
-    email = StringField('Enter your Email')
+    name = StringField('Name', validators=[DataRequired(), Length(3, 15, message='between 3 to 15 characters')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(5), EqualTo('confirm', message='password must match')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
     submit = SubmitField('Register')
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,20 +1,25 @@
-# INCREMENTAL PYTHON CODE FOR SECTION 11: LECTURE : 41
-# REGISTRATION AND CSRF PROTECTION
-
-
-from flask import render_template, request
+from flask import render_template, flash, redirect, url_for
 from app.auth.forms import RegistrationForm
 from app.auth import authentication as at
+from app.auth.models import User
+from app import login_manager
 
 
 @at.route('/register', methods=['GET', 'POST'])
 def register_user():
-    name = None
-    email = None
     form = RegistrationForm()
 
-    if request.method == 'POST':
-        name = form.name.data
-        email = form.email.data
+    if form.validate_on_submit():
+        User.create_user(
+            user=form.name.data,
+            email=form.email.data,
+            password=form.password.data)
+        flash('Registration Successful')
+        return redirect(url_for('authentication.login_user'))
+    return render_template('registration.html', form=form)
 
-    return render_template('registration.html', form=form, name=name, email=email)
+@login_manager.user_loader
+@at.route('/login', methods=['GET', 'POST'])
+def login_user():
+    return render_template('login.html')
+
