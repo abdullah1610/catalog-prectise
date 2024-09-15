3611f83aba7b5e31207ca51c8da1237ad06d2424
abdullah1610 <shaikhabdullah1610@gmail.com>
2024-09-15 20:25:37 +0530

    43. creating users in the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
+# app/__init__.py (ROOT PACKAGE)
+# PYTHON CODE FOR SECTION : 11 LECTURE : 42 (flask-bcrypt and flask-login)
+
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+# from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 # migrate = Migrate()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
 
 def create_app(config_type):
     app = Flask(__name__)
@@ -12,6 +23,9 @@ def create_app(config_type):
     app.config.from_pyfile(configuration)
     db.init_app(app)
     # migrate.init_app(app, db)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
@@ -20,6 +34,3 @@ def create_app(config_type):
     app.register_blueprint(authentication)
 
     return app
-
-
-
